c3631c63584732e4c2a60678165bf59d5a12113b
forhad36 <forhad.sec@gmail.com>
2022-07-19 02:47:17 +0600

    add event by shared function

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+function updateProductNumber(product,price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if(isIncreasing==true){
+        productNumber.value= parseInt(productNumber)+1;
+    }
+    else if(productNumber>0){
+        productNumber.value= parseInt(productNumber)-1;
+    }
+    productInput.value = productNumber;
+    const productTotal =document.getElementById(product + '-total');
+    productTotal.innerText = productNumber *price;
+}
+
 document.getElementById('case-plus').addEventListener('click',function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value= parseInt(caseNumber)+1;
+    updateProductNumber('case',59,true);
+    
 });
 document.getElementById('case-minus').addEventListener('click',function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value= parseInt(caseNumber)-1;
-})
+    updateProductNumber('case',59,false);
+    
+});
+//phone increse decress event
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,true);
+    
+});
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false);
+    
+});
